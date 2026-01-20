f1dba3f81c4f5dbf540059babb3f63d58f3116d1
Matheus Roberto Alves Andrade <matheus.andrade@abiquim.org.br>
2026-01-20 17:20:38 -0300

    adaptando collect_data para captar dados históricos

--- src/collect_data.py
+++ src/collect_data.py
@@ -19,7 +19,7 @@ class DataCollector:
         self.s3_prefix = s3_prefix
         self.s3_client = boto3.client('s3')
 
-    def fetch_data(self, ticker: str) -> pd.DataFrame:
+    def fetch_data(self, ticker: str, period: str) -> pd.DataFrame:
         """
         Coleta dos dados a partir do ticker.
 
@@ -28,7 +28,7 @@ class DataCollector:
         """
         try:
             yf_instance = yf.Ticker(ticker=ticker)
-            df = yf_instance.history(period='1d')
+            df = yf_instance.history(period=period)
             print(f'Dados coletados para: {ticker} (Registros: {len(df)})')
             return df
         except Exception as e:
@@ -48,31 +48,36 @@ class DataCollector:
             return print(f'Nenhum dado para salvar para: {ticker}')
         pass
             
-        buffer = BytesIO()
         df.reset_index(inplace=True)
         df['Date'] = pd.to_datetime(df['Date']).dt.date
-        df.to_parquet(buffer, index=False)
-        buffer.seek(0)
 
-        date = df['Date'].iloc[0]
-        year = date.year
-        month = date.month
-        day = date.day
+        for index, row in df.iterrows():
+            
+            df_single_day = df.iloc[[index]]
+            
+            buffer = BytesIO()
+            df_single_day.to_parquet(buffer, index=False)
+            buffer.seek(0)
 
-        s3_key = f"{self.s3_prefix}ticker={ticker}/year={year}/month={month}/day={day}/{ticker}.parquet"
+            date = row['Date']
+            year = date.year
+            month = date.month
+            day = date.day
 
-        try:
-            self.s3_client.upload_fileobj(buffer, self.bucket_name, s3_key)
-            print(f'Arquivo salvo no S3: s3://{self.bucket_name}/{s3_key}')
-        except ClientError as e:
-            print(f'Erro ao salvar arquivo no S3 para {ticker}: {e}')
-        
+            s3_key = f"{self.s3_prefix}ticker={ticker}/year={year}/month={month}/day={day}/{ticker}.parquet"
+
+            try:
+                self.s3_client.upload_fileobj(buffer, self.bucket_name, s3_key)
+                print(f'Arquivo salvo no S3: s3://{self.bucket_name}/{s3_key}')
+            except ClientError as e:
+                print(f'Erro ao salvar arquivo no S3 para {ticker}: {e}')
+            
     def run(self):
         """
         Executa o pipeline completo: coleta e salva
         """
         for ticker in self.tickers:
-            df = self.fetch_data(ticker)
+            df = self.fetch_data(ticker, period='30d')
             self.save_to_s3(df=df, ticker=ticker)
         
 if __name__ == '__main__':
